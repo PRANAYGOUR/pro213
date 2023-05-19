84e45c0277b21f48370ab15e9e71e4a89be7e131
PRANAYGOUR <76206607+PRANAYGOUR@users.noreply.github.com>
2023-05-19 12:51:03 +0530

    Add files via upload

---
+++ remote_Server.py
@@ -0,0 +1,80 @@
+import socket
+from  threading import Thread
+from pynput.mouse import Button, Controller
+from screeninfo import get_monitors
+from pynput.keyboard import Key, Controller
+
+
+
+
+
+SERVER = None
+PORT = 8000
+IP_ADDRESS = input("Enter your computer IP ADDR : ").strip()
+screen_width = None
+screen_height = None
+global keyboard
+
+keyboard = Controller()
+
+
+
+
+def getDeviceSize():
+    global screen_width
+    global screen_height
+    for m in get_monitors():
+        screen_width = int(str(m).split(",")[2].strip().split('width=')[1])
+        screen_height = int(str(m).split(",")[3].strip().split('height=')[1])
+
+
+
+
+
+
+def acceptConnections():
+    global SERVER
+
+    while True:
+        client_socket, addr = SERVER.accept()
+
+        print(f"Connection established with {client_socket} : {addr}")
+        thread1 = Thread(target=recvmsg , args=(client_socket,))
+        thread1.start()
+
+def recvmsg(clinetSocket):
+    global keyboard
+    
+    while True:
+        try:
+            message = clinetSocket.recv(2048).decode()
+            if(message):
+                keyboard.press(keyboard)
+                keyboard.release(keyboard)
+                print(message)
+        except Exception as Error:
+            pass
+
+    
+
+
+def setup():
+    print("\n\t\t\t\t\t*** Welcome To Remote Mouse ***\n")
+
+
+    global SERVER
+    global PORT
+    global IP_ADDRESS
+
+
+    SERVER = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    SERVER.bind((IP_ADDRESS, PORT))
+
+    SERVER.listen(10)
+
+    print("\t\t\t\tSERVER IS WAITING FOR INCOMMING CONNECTIONS...\n")
+
+    getDeviceSize()
+    acceptConnections()
+
+setup()
